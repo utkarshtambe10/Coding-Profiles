f78ff37b30108bc76b1389bcaad3c11456856e1a
Utkarsh Tambe <utkarsh.tambe33@gmail.com>
2022-09-26 20:01:07 +0530

    adding navbar to the webpage

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,12 @@
+export const MenuItems = [
+    {
+        title: 'GitHub',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'LinkedIn',
+        url: '#',
+        cName: 'nav-links'
+    }
+]
